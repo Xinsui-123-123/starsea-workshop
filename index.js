16296e07f83fd8806474da8053b68dadc0bdcffd
suyunsu797 <crookerjimenes213@gmail.com>
2026-08-22 13:49:47 +0800

    Update workshop to dev.28

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ import './src/workshop.js';
   }
 
 
-  var XYWS_EXT_VERSION = '2.0.0-dev.27';
+  var XYWS_EXT_VERSION = '2.0.0-dev.28';
   var settingsMounted = false;
 
   async function xywsRequestHeaders() {
